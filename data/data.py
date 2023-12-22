a8e019ea08ba0dc6886982b4f217dea17fb0fd92
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2023-12-22 23:58:57 +0530

    Add files via upload

---
+++ data/data.py
@@ -0,0 +1,39 @@
+class Data:
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    username01 = "Admin"
+    password01 = "admin123"
+    username02 = "Admin"
+    password02 = "invalid password"
+    upload_img = 'C:/Projects/Project-Guvi01/Image/pic01.jpg'
+    fst_name = "Jack"
+    mid_name = "Z"
+    lst_name = "Smith"
+    emp_id = "9976"
+    user_name = "jacksmith"
+    pass_word = "Qwerty@123"
+    con_pass = "Qwerty@123"
+
+
+class Personal:
+    # Personal Details
+    dl_no = 'DL-12345'
+    exp_dl = '2020-09-06'
+    ssn_no = 'SSN-123456J'
+    sin_no = 'SIN-123456J'
+    dob = '1999-10-10'
+    miltary = 'Yes'
+
+
+class Edit:
+    # Edit Details
+    dl_no_ed = 'DL-1259'
+    exp_dl_ed = '2021-10-09'
+    ssn_no_ed = 'SSN-1259J'
+    sin_no_ed = 'SIN-1259J'
+    dob_ed = '2023-12-01'
+    miltary_ed = 'No'
+    res = 'C:/Projects/Project-Guvi01/Image/resume.pdf'
+
+
+
